c549cde9df655c5380dfbca8f2868d41d225974f
Temirlan Baibolov <bthero09@gmail.com>
2021-12-16 09:49:01 +0100

    compress images

--- src/components/CarouselData.tsx
+++ src/components/CarouselData.tsx
@@ -2,27 +2,27 @@ import { SlideProps } from "./types";
 
 const list : SlideProps[] = [
   {
-    imageUrl: "/assets/2016-winners.jpeg",
+    imageUrl: "/assets/2016-winners.webp",
     heading: "Finals 2016",
     description: "May 15-20, Phuket"
   },
   {
-    imageUrl: "/assets/2017-winners.jpeg",
+    imageUrl: "/assets/2017-winners.webp",
     heading: "Finals 2017",
     description: "May 20-25, Rapid City"
   },
   {
-    imageUrl: "/assets/2018-winners.jpeg",
+    imageUrl: "/assets/2018-winners.webp",
     heading: "Finals 2018",
     description: "April 15-20, Peking University"
   },
   {
-    imageUrl: "/assets/2019-winners.jpeg",
+    imageUrl: "/assets/2019-winners.webp",
     heading: "Finals 2019",
     description: "March 31 - April 5, Porto"
   },
   {
-    imageUrl: "/assets/2020-winners.jpeg",
+    imageUrl: "/assets/2020-winners.webp",
     heading: "Finals 2020",
     description: "October 1-5, Moscow (2021)"
   }
